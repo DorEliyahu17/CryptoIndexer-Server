75c9046876f3b6590a96a65f8f3e68faa09cca62
Dor Eliyahu <doreliyahu17@gmail.com>
2022-05-22 03:01:35 +0300

    fixed backtest new custom index response

--- BacktestNewCustomIndex.py
+++ BacktestNewCustomIndex.py
@@ -7,20 +7,20 @@ try:
     arg_ind: Dict[str, float] = json.loads(argv[1])
 
     index = Index('tmp', 'tmp')
-    
+
     for symbol, weight in arg_ind.items():
         index.AddSymbol(symbol.replace('"', ''), float(weight))
 
     backtest_res = index.Backtest(1000, 0.004)
 
     dic_to_ret = {
-        "success": True, 
-        "data": 
+        "success": True,
+        "data":
             {
                 "dates": [str(x) for x in backtest_res[0]],
-                "balance progress": backtest_res[1]
+                "balance_progress": backtest_res[1]
             }
-        }
+    }
 except Exception as e:
     dic_to_ret = {"success": False, "data": str(e)}
 
