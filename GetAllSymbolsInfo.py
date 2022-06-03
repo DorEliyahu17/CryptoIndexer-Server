34f9cb0d0f557b220b2f83558e91834e46b3203d
Dor Eliyahu <doreliyahu17@gmail.com>
2022-06-03 22:04:00 +0300

    waiting to liavs fix

--- GetAllSymbolsInfo.py
+++ GetAllSymbolsInfo.py
@@ -1,3 +1,4 @@
+from base64 import encode
 from DataFetcher import GetAllSymbolsInfo
 
 try:
@@ -7,5 +8,8 @@ try:
     }
 except Exception as e:
     dict_to_ret = {"success": False, "data": str(e)}
+
+with open('res.txt', 'w', encoding='utf8') as f:
+    f.write(str(str(dict_to_ret).encode('utf8')))
 
 print(dict_to_ret)
