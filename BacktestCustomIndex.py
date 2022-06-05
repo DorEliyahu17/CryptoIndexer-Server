b50c992e997f2471c87be603afea9b513563350a
Dor Eliyahu <doreliyahu17@gmail.com>
2022-06-05 16:18:34 +0300

    fixed backtest new index bug

--- BacktestCustomIndex.py
+++ BacktestCustomIndex.py
@@ -4,16 +4,18 @@ from Index import Index
 from DataFetcher import GetHistoricalPriceData
 import BacktestStatisticsFunctions as bsf
 
+
 def GenerateSymbolRes(symbol: str, initial_balance: float) -> dict:
     res = {}
     res["symbol"] = symbol
     price_action = GetHistoricalPriceData(symbol)
     res["dates"] = [str(x) for x in price_action["Close time"]]
-    res["balance_progress"] = bsf.BacktestHODL(price_action["Close"], initial_balance)
+    res["balance_progress"] = bsf.BacktestHODL(
+        price_action["Close"], initial_balance)
     res["roi"] = bsf.ROI(res["balance_progress"][-1], initial_balance)
     return res
 
-print(argv[1])
+
 try:
     arg_ind: dict[str, float] = json.loads(argv[1])
     try:
@@ -24,11 +26,11 @@ try:
     index = Index('tmp', 'tmp')
 
     for symbol, weight in arg_ind.items():
-        print(symbol)
         index.AddSymbol(symbol.replace('"', ''), float(weight))
 
     backtest_res = index.Backtest(initial_balance, 0.001)
-    symbols_res = [GenerateSymbolRes(x, initial_balance) for x in index.symbols_weights.keys()]
+    symbols_res = [GenerateSymbolRes(x, initial_balance)
+                   for x in index.symbols_weights.keys()]
 
     dic_to_ret = {
         "success": True,
@@ -45,7 +47,6 @@ try:
 
 except Exception as e:
     dic_to_ret = {"success": False, "data": str(e)}
-
 
 
 print(json.dumps(dic_to_ret))
